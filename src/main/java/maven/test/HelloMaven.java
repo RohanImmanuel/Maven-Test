ada3f6dd865182f89dc3a7b4b1d2960c6f078406
Rohan Immanuel <i25.rohan@gmail.com>
2019-10-01 18:48:40 +0530

    updated second line of o/p

--- src/main/java/maven/test/HelloMaven.java
+++ src/main/java/maven/test/HelloMaven.java
@@ -4,7 +4,7 @@ public class HelloMaven {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("	- Maven");
+		System.out.println("	- file edited");
 	}
 
 }
